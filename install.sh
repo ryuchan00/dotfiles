f536dc7b8093b4f76f228e26e5e8d76a4f401dad
ryuchan00 <leokun0210@gmail.com>
2019-09-01 15:19:44 +0900

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ $# != 1 ]; then
+    echo 'missing number of arguments.'
+    echo 'correct number is 1.'
+    exit 1
+fi
 
 # ref: https://qiita.com/yudoufu/items/48cb6fb71e5b498b2532
 # ghqで制御しているので、このファイルの実行ディレクトリをDOTFILESとする
@@ -13,12 +19,74 @@ function avoid_same_file() {
   fi
 }
 
-avoid_same_file '.gitconfig'
-avoid_same_file '.gitignore_global'
-avoid_same_file '.zshrc'
-avoid_same_file '.tmux.conf'
 
-ln -fs $DOTFILES/.gitconfig_$1 ./.gitconfig
-ln -fs $DOTFILES/.gitignore_global
-ln -fs $DOTFILES/.zshrc_$1 ./.zshrc
-ln -fs $DOTFILES/.tmux.conf
+echo "update .gitconfig?[yn]"
+read answer
+case $answer in
+    y)
+        avoid_same_file '.gitconfig'
+        ln -fs $DOTFILES/.gitconfig.$1 ./.gitconfig
+        ;;
+    n)
+        ;;
+    *)
+        echo -e "cannot understand $answer."
+        ;;
+esac
+
+echo "update .gitconfig_global?[yn]"
+read answer
+case $answer in
+    y)
+        avoid_same_file '.gitignore_global'
+        ln -fs $DOTFILES/.gitignore_global
+        ;;
+    n)
+        ;;
+    *)
+        echo -e "cannot understand $answer."
+        ;;
+esac
+
+echo "update .zshrc?[yn]"
+read answer
+case $answer in
+    y)
+        avoid_same_file '.zshrc'
+        ln -fs $DOTFILES/.zshrc.$1 ./.zshrc
+        ;;
+    n)
+        ;;
+    *)
+        echo -e "cannot understand $answer."
+        ;;
+esac
+
+echo "update .tmux_conf?[yn]"
+read answer
+case $answer in
+    y)
+        avoid_same_file '.tmux.conf'
+        ln -fs $DOTFILES/.tmux.conf
+        ;;
+    n)
+        ;;
+    *)
+        echo -e "cannot understand $answer."
+        ;;
+esac
+
+echo "update .vimrc?[yn]"
+read answer
+case $answer in
+    y)
+        avoid_same_file '.vimrc'
+        ln -fs $DOTFILES/.vimrc
+        ;;
+    n)
+        ;;
+    *)
+        echo -e "cannot understand $answer."
+        ;;
+esac
+
