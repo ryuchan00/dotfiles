87b8f735b8ec18d76601c950212a4c93c46e54d7
ryuchun00 <leokun0210@gmail.com>
2022-06-25 18:46:24 +0900

    update

--- install.sh
+++ install.sh
@@ -15,17 +15,18 @@ DOTFILES=$(
 
 cd ~/
 now=$(date '+%Y%m%d%H%M%S')
+mkdir dotfile_history
 
 function avoid_same_file() {
     if [[ -e $1 ]]; then
-        mv $1 $1_$now
+        mv $1 dotfile_history/$1_$now
     fi
 }
 
-echo "update .gitconfig?[yn]"
+echo "update .gitconfig?[Y/n]"
 read answer
 case $answer in
-y)
+[Yy]*)
     avoid_same_file '.gitconfig'
     ln -fs $DOTFILES/.gitconfig.$1 ./.gitconfig
     ;;
@@ -36,10 +37,10 @@ n) ;;
     ;;
 esac
 
-echo "update .gitconfig_global?[yn]"
+echo "update .gitconfig_global?[Y/n]"
 read answer
 case $answer in
-y)
+[Yy]*)
     avoid_same_file '.gitignore_global'
     ln -fs $DOTFILES/.gitignore_global
     ;;
@@ -50,10 +51,10 @@ n) ;;
     ;;
 esac
 
-echo "update .zshrc?[yn]"
+echo "update .zshrc?[Y/n]"
 read answer
 case $answer in
-y)
+[Yy]*)
     avoid_same_file '.zshrc'
     avoid_same_file '.zshrc.base'
     ln -fs $DOTFILES/.zshrc.base ./.zshrc.base
@@ -66,10 +67,10 @@ n) ;;
     ;;
 esac
 
-echo "update .tmux_conf?[yn]"
+echo "update .tmux_conf?[Y/n]"
 read answer
 case $answer in
-y)
+[Yy]*)
     avoid_same_file '.tmux.conf'
     ln -fs $DOTFILES/.tmux.conf.$1 ./.tmux.conf
     ;;
@@ -80,10 +81,10 @@ n) ;;
     ;;
 esac
 
-echo "update .vimrc?[yn]"
+echo "update .vimrc?[Y/n]"
 read answer
 case $answer in
-y)
+[Yy]*)
     avoid_same_file '.vimrc'
     ln -fs $DOTFILES/.vimrc
     ;;
